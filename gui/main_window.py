02be49ad832849181a442ad92e30399434269eb3
Maddie Wagner <musomaddy@gmail.com>
2022-06-05 10:46:22 +1000

    The main window successfully displays the pattern view overlay layout

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,13 +1,25 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QStackedWidget, QWidget
 
 import resources.gui_strings as s
 from gui.patterns_layout.pattern_selector import PatternSelectorLayout
+from gui.patterns_layout.pattern_view_overview import PatternViewOverviewLayout
 
 
-class MainWindow(QWidget):
+class MainWindow(QStackedWidget):
+    """ Manages the main window for the GUI. This is where all layouts
+    eventually stem from.
+
+    """
+
     def __init__(self, istest=False):
         """ Initialises the main window. This is the GUI contain that contains
         all the elements of the program.
+
+        Parameters:
+            selector_widget     QWidget     a basic widget containing the
+                                                layout for the parent selector
+            view_widget         QWidget     a default widget containing the
+                                                layout for the pattern viewer
         """
         super().__init__()
 
@@ -15,10 +27,16 @@ class MainWindow(QWidget):
         if not istest:
             self.showMaximized()
         self.setWindowTitle(s.program_title())
+        self.selector_widget = QWidget()
+        self.selector_widget.setLayout(PatternSelectorLayout(self))
+        self.view_widget = QWidget()
 
-        self.setLayout(PatternSelectorLayout(self))
+        self.addWidget(self.selector_widget)
+        self.addWidget(self.view_widget)
 
     def pattern_chosen(self, pattern_name):
         """ Loads the default display window for this pattern.
         """
-        pass
+        self.view_widget.setLayout(
+            PatternViewOverviewLayout(pattern_name, self))
+        self.setCurrentWidget(self.view_widget)
--- tests/gui/test_main_window.py
+++ tests/gui/test_main_window.py
@@ -7,12 +7,26 @@ from gui.main_window import MainWindow
 
 
 @patch("gui.main_window.PatternSelectorLayout")
-def test_init(child_mock, qtbot):
-    child_mock.return_value = QVBoxLayout()
-    test_window = MainWindow(True)
+def test_init(layout_child_mock, qtbot):
+    layout_child_mock.return_value = QVBoxLayout()
+    test_widget = MainWindow(True)
+    qtbot.addWidget(test_widget)
 
-    qtbot.addWidget(test_window)
+    assert test_widget.windowTitle() == s.program_title()
+    assert test_widget.layout().count() == 2
 
-    print(test_window)
-    assert test_window.windowTitle() == s.program_title()
-    assert child_mock.call_count == 1
+    layout_child_mock.assert_called_once_with(test_widget)
+    assert test_widget.currentIndex() == 0
+
+
+@patch("gui.main_window.PatternSelectorLayout")
+@patch("gui.main_window.PatternViewOverviewLayout")
+def test_pattern_chosen(view_child_mock, layout_child_mock, qtbot):
+    layout_child_mock.return_value = QVBoxLayout()
+    view_child_mock.return_value = QVBoxLayout()
+    test_widget = MainWindow(True)
+    qtbot.addWidget(test_widget)
+    test_widget.pattern_chosen("Testing")
+
+    view_child_mock.assert_called_once_with("Testing", test_widget)
+    assert test_widget.currentIndex() == 1
