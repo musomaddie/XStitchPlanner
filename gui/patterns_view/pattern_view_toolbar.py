a1e3dbc8106c7e2b7d6ae014c330728f0b3f8a19
Maddie Wagner <musomaddy@gmail.com>
2022-06-08 20:02:22 +1000

    Added toolbar

---
+++ gui/patterns_view/pattern_view_toolbar.py
@@ -0,0 +1,41 @@
+from PyQt6.QtCore import QSize
+from PyQt6.QtGui import QAction, QIcon
+from PyQt6.QtWidgets import QToolBar
+
+
+class PatternViewToolBar(QToolBar):
+    # TODO: potentially move where this is located
+    """ Contains all the buttons in the view pattern tool bar.
+
+   +-------------------------------------------------------------------+
+   |        |       |                                                  |
+   | COLOUR | B / W |                                                  |
+   |        |       |                                                  |
+   +-------------------------------------------------------------------+
+
+    Parameters:
+        parent      ViewHierarchy           the parent that this attaches to
+        data_model  PatternDisplayGridModel the model managing the visible
+                                                pattern
+        colour_on   CellColourSwitcher      an action that adds a colour
+                                                background
+        colour_off  CellColourSwitcher      an action that removes a colour
+                                                background
+    """
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.parent = parent
+        self.data_model = None
+        self.setIconSize(QSize(16, 16))
+
+        # TODO: if I also have a menu bar it's going to be hard to access
+        #  these icons so fix it in this case
+        self.colour_on = QAction(
+            QIcon("resources/gui_icons/color-swatch.png"),
+            "Enable Colour Background", parent)
+        self.colour_off = QAction(
+            QIcon("resources/gui_icons/gradient.png"),
+            "Disable Colour Background", parent)
+        self.addAction(self.colour_on)
+        self.addAction(self.colour_off)
