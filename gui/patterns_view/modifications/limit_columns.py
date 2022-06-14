b8e2e8fc8ca88469ae42e31298fd1bff86e1c204
Madeleine Wagner <musomaddy@gmail.com>
2022-06-14 19:44:50 +1000

    Now the column limiter selector updates

--- gui/patterns_view/modifications/limit_columns.py
+++ gui/patterns_view/modifications/limit_columns.py
@@ -1,8 +1,7 @@
 from enum import Enum
 
-from PyQt6.QtWidgets import (QComboBox, QHBoxLayout, QLabel, QStackedLayout,
-                             QVBoxLayout,
-                             QWidget)
+from PyQt6.QtWidgets import (
+    QComboBox, QHBoxLayout, QLabel, QStackedLayout, QVBoxLayout, QWidget)
 
 
 class ColumnLimiterMode(Enum):
@@ -15,15 +14,24 @@ class ColumnLimiterMode(Enum):
 class LimitColumnsDropDown(QComboBox):
     """ Contains the dropdown with the choice of which column limiter to use
     """
+    parent: 'LimitColumnsLayout'
+    value_selector: 'LimitColumnsValueSelector'
     options: list[ColumnLimiterMode]
-    selected_mode: ColumnLimiterMode
 
-    def __init__(self):
+    def __init__(
+            self,
+            value_selector: 'LimitColumnsValueSelector',
+            parent: 'LimitColumnsLayout' = None):
         super().__init__()
+        self.parent = parent
+        self.value_selector = value_selector
         self.options = list(ColumnLimiterMode)
         self.addItems([opt.value for opt in self.options])
-        self.selected_mode = self.options[0]
-        # self.activated.connect(self.update_currently_selected)
+        self.activated.connect(self.update_currently_selected)
+
+    def update_currently_selected(self):
+        self.value_selector.change_selected(
+            self.options[self.currentIndex()])
 
 
 class LimitColumnsValueSelector(QStackedLayout):
@@ -34,36 +42,49 @@ class LimitColumnsValueSelector(QStackedLayout):
     TO_COLUMN:       | TO xx (use_selection) |
     """
     parent: 'LimitColumnsLayout'
-    no_selector_layout: QWidget
-    between_columns_layout: QHBoxLayout
-    from_column_layout: QHBoxLayout
-    to_column_layout: QHBoxLayout
+    selection_dictionary: dict[ColumnLimiterMode, QWidget]
+
+    # TODO: not sure if these should be saved as class variables too
+    # no_selector_layout: QWidget
+    # between_columns_layout: QHBoxLayout
+    # from_column_layout: QHBoxLayout
+    # to_column_layout: QHBoxLayout
 
     def __init__(self, parent: 'LimitColumnsLayout' = None):
         super().__init__()
         self.parent = parent
 
-        self.no_selector_layout = QWidget()
-        self.addWidget(self.no_selector_layout)
+        no_selector_layout_widget = QWidget()
+        self.addWidget(no_selector_layout_widget)
 
-        self.between_columns_layout = QHBoxLayout()
-        self.between_columns_layout.addWidget(QLabel("between columns"))
-        self.between_columns_layout_widget = QWidget()
-        self.between_columns_layout_widget.setLayout(
-            self.between_columns_layout)
-        self.addWidget(self.between_columns_layout_widget)
+        between_columns_layout = QHBoxLayout()
+        between_columns_layout.addWidget(QLabel("between columns"))
+        between_columns_layout_widget = QWidget()
+        between_columns_layout_widget.setLayout(
+            between_columns_layout)
+        self.addWidget(between_columns_layout_widget)
 
-        self.from_column_layout = QHBoxLayout()
-        self.from_column_layout.addWidget(QLabel("from column"))
-        self.from_column_layout_widget = QWidget()
-        self.from_column_layout_widget.setLayout(self.from_column_layout)
-        self.addWidget(self.from_column_layout_widget)
+        from_column_layout = QHBoxLayout()
+        from_column_layout.addWidget(QLabel("from column"))
+        from_column_layout_widget = QWidget()
+        from_column_layout_widget.setLayout(from_column_layout)
+        self.addWidget(from_column_layout_widget)
 
-        self.to_column_layout = QHBoxLayout()
-        self.to_column_layout.addWidget(QLabel("to column"))
-        self.to_column_layout_widget = QWidget()
-        self.to_column_layout_widget.setLayout(self.to_column_layout)
-        self.addWidget(self.to_column_layout_widget)
+        to_column_layout = QHBoxLayout()
+        to_column_layout.addWidget(QLabel("to column"))
+        to_column_layout_widget = QWidget()
+        to_column_layout_widget.setLayout(to_column_layout)
+        self.addWidget(to_column_layout_widget)
+
+        self.selection_dictionary = {
+            ColumnLimiterMode.NO_SELECTOR: no_selector_layout_widget,
+            ColumnLimiterMode.BETWEEN_COLUMNS: between_columns_layout_widget,
+            ColumnLimiterMode.FROM_COLUMN: from_column_layout_widget,
+            ColumnLimiterMode.TO_COLUMN: to_column_layout_widget
+        }
+
+    def change_selected(self, mode: ColumnLimiterMode) -> None:
+        self.setCurrentWidget(self.selection_dictionary[mode])
 
 
 class LimitColumnsLayout(QVBoxLayout):
@@ -90,10 +111,11 @@ class LimitColumnsLayout(QVBoxLayout):
         self.parent = parent
 
         self.title = QLabel("Limit pattern via columns")
-        self.mode_selector_dd = LimitColumnsDropDown()
+        self.value_selector = LimitColumnsValueSelector()
+        self.mode_selector_dd = LimitColumnsDropDown(self.value_selector,
+                                                     self)
         self.addWidget(self.title)
         self.addWidget(self.mode_selector_dd)
-        self.value_selector = LimitColumnsValueSelector()
         value_selector_layout_widget = QWidget()
         value_selector_layout_widget.setLayout(self.value_selector)
         self.addWidget(value_selector_layout_widget)
--- tests/gui/patterns_view/modifications/test_limit_columns.py
+++ tests/gui/patterns_view/modifications/test_limit_columns.py
@@ -1,5 +1,6 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, call, patch
 
+import pytest
 from PyQt6.QtWidgets import QComboBox, QStackedLayout, QWidget
 
 from gui.patterns_view.modifications.limit_columns import (
@@ -9,6 +10,7 @@ from gui.patterns_view.modifications.limit_columns import (
 FILE_LOC = "gui.patterns_view.modifications.limit_columns."
 
 
+# VALUE_SELECTOR
 def test_init_value_selector(qtbot):
     test_widget = QWidget()
     value_selector = LimitColumnsValueSelector()
@@ -16,11 +18,30 @@ def test_init_value_selector(qtbot):
     qtbot.addWidget(test_widget)
 
     assert test_widget.layout().count() == 4
+    assert value_selector.currentIndex() == 0
 
 
+@pytest.mark.parametrize(
+    ("new_mode", "num_child"),
+    [(ColumnLimiterMode.NO_SELECTOR, 0),
+     (ColumnLimiterMode.BETWEEN_COLUMNS, 1),
+     (ColumnLimiterMode.FROM_COLUMN, 2),
+     (ColumnLimiterMode.TO_COLUMN, 3)]
+)
+def test_change_selected(new_mode, num_child, qtbot):
+    test_widget = QWidget()
+    value_selector = LimitColumnsValueSelector()
+    test_widget.setLayout(value_selector)
+    qtbot.addWidget(test_widget)
+
+    value_selector.change_selected(new_mode)
+    assert value_selector.currentIndex() == num_child
+
+
+# LAYOUT
 @patch(f"{FILE_LOC}LimitColumnsValueSelector")
 @patch(f"{FILE_LOC}LimitColumnsDropDown")
-def test_init(dropdown_mock, selector_mock, qtbot):
+def test_init_layout(dropdown_mock, selector_mock, qtbot):
     dropdown_mock.return_value = QComboBox()
     selector_mock.return_value = QStackedLayout()
     test_widget = QWidget()
@@ -33,13 +54,31 @@ def test_init(dropdown_mock, selector_mock, qtbot):
     assert selector_mock.called
 
 
+# DROPDOWN
 def test_init_dropdown(qtbot):
-    opt_dropdown = LimitColumnsDropDown()
+    opt_dropdown = LimitColumnsDropDown(MagicMock())
     qtbot.addWidget(opt_dropdown)
 
-    assert opt_dropdown.selected_mode == ColumnLimiterMode.NO_SELECTOR
+    assert opt_dropdown.currentIndex() == 0
     assert opt_dropdown.options == [
         ColumnLimiterMode.NO_SELECTOR,
         ColumnLimiterMode.BETWEEN_COLUMNS,
         ColumnLimiterMode.FROM_COLUMN,
         ColumnLimiterMode.TO_COLUMN]
+
+
+@pytest.mark.parametrize(
+    ("index", "expected_argument"),
+    [(0, ColumnLimiterMode.NO_SELECTOR),
+     (1, ColumnLimiterMode.BETWEEN_COLUMNS),
+     (2, ColumnLimiterMode.FROM_COLUMN),
+     (3, ColumnLimiterMode.TO_COLUMN)]
+)
+def test_update_currently_selected(index, expected_argument):
+    value_selector = MagicMock()
+    opt_dropdown = LimitColumnsDropDown(value_selector)
+
+    opt_dropdown.setCurrentIndex(index)
+    opt_dropdown.update_currently_selected()
+    assert value_selector.mock_calls == [
+        call.change_selected(expected_argument)]
