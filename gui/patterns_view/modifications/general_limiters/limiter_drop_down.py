302118eb7348910773ede578939cbc533ac79bf3
Madeleine Wagner <musomaddy@gmail.com>
2022-06-17 18:41:41 +1000

    Added generic limiter dropdown box

---
+++ gui/patterns_view/modifications/general_limiters/limiter_drop_down.py
@@ -0,0 +1,35 @@
+from PyQt6.QtWidgets import QComboBox
+
+from gui.patterns_view.modifications.general_limiters.limiter_direction import \
+    LimiterDirection
+from gui.patterns_view.modifications.general_limiters.limiter_mode import \
+    LimiterMode
+
+
+class LimiterDropDown(QComboBox):
+    """ Contains the dropdown with the choice of which column limiter to use
+    """
+    parent: 'LimiterOverlay'
+    limiter_direction: LimiterDirection
+    value_selector_stack: 'LimiterSelectorStack'
+
+    def __init__(
+            self,
+            limiter_direction: LimiterDirection,
+            value_selector_stack: 'LimiterSelectorStack',
+            parent: 'LimiterOverlay' = None):
+        super().__init__()
+        self.parent = parent
+        self.options = list(LimiterMode)
+        self.value_selector_stack = value_selector_stack
+
+        self.addItems(
+            [f"{opt.value} {limiter_direction.value}s".title()
+             if opt == LimiterMode.BETWEEN or opt == LimiterMode.NO_SELECTOR
+             else f"{opt.value.title()} {limiter_direction.value}".title()
+             for opt in self.options])
+        self.activated.connect(self.update_currently_selected)
+
+    def update_currently_selected(self):
+        self.value_selector_stack.change_selected(
+            self.options[self.currentIndex()])
---
+++ tests/gui/patterns_view/modifications/limiters/test_limiter_dropdown.py
@@ -0,0 +1,37 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from gui.patterns_view.modifications.general_limiters.limiter_direction import \
+    LimiterDirection
+from gui.patterns_view.modifications.general_limiters.limiter_drop_down import \
+    LimiterDropDown
+from gui.patterns_view.modifications.general_limiters.limiter_mode import \
+    LimiterMode
+
+
+@pytest.mark.parametrize(
+    "direction", [LimiterDirection.COLUMN, LimiterDirection.ROW]
+)
+def test_init_dropdown(direction, qtbot):
+    dropdown = LimiterDropDown(direction, MagicMock())
+    qtbot.addWidget(dropdown)
+
+    assert dropdown.currentIndex() == 0
+    assert dropdown.options == [
+        LimiterMode.NO_SELECTOR,
+        LimiterMode.BETWEEN,
+        LimiterMode.FROM,
+        LimiterMode.TO]
+
+    assert dropdown.count() == 4
+    if direction == LimiterDirection.ROW:
+        assert dropdown.itemText(0) == "No Rows"
+        assert dropdown.itemText(1) == "Between Rows"
+        assert dropdown.itemText(2) == "From Row"
+        assert dropdown.itemText(3) == "To Row"
+    else:
+        assert dropdown.itemText(0) == "No Columns"
+        assert dropdown.itemText(1) == "Between Columns"
+        assert dropdown.itemText(2) == "From Column"
+        assert dropdown.itemText(3) == "To Column"
