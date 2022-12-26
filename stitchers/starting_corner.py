477fec25c50ae0ccc1f4d8daa320ae54c62994d1
Madeleine Wagner <musomaddy@gmail.com>
2022-12-27 10:00:38 +1100

    Added a generate function to iterate over all cells that are the same colour within a row.
    Included refactoring the pattern generator class to include more helper methods and associated tests.

--- stitchers/starting_corner.py
+++ stitchers/starting_corner.py
@@ -26,6 +26,7 @@ class StartingCorner:
 
 
 class PatternGenerator:
+    # TODO: move to a better file?? - the color seems dependent on the parking approach.
     vertical_dir: VerticalDirection
     horizontal_dir: HorizontalDirection
     pattern: list[list['StitchingCell']]
@@ -36,26 +37,61 @@ class PatternGenerator:
         self.horizontal_dir = starting_corner.horizontal
         self.vertical_dir = starting_corner.vertical
         self.pattern = stitching_pattern
-        self.current_x = 0 if self.horizontal_dir == HorizontalDirection.LEFT else len(self.pattern[0]) - 1
+        self.current_x = self._horizontal_idx_start()
         self.current_y = 0 if self.vertical_dir == VerticalDirection.TOP else len(self.pattern) - 1
 
-    # Create slightly different generates based on the need(?) This will iterate through the current row - and move to
-    # the next
     def move_through_row(self):
+        """ Iterates through the entire row (all cells). Sets up the indices to iterate through the next row. """
+        # TODO: should I handle stitched cells being part of this? (I don't want to return them if so)
+        for value in self._row_generator():
+            yield value
+        if not self._within_bounds():
+            raise StopIteration
+        self._update_y()
+
+    def move_through_colour_in_rows(self):
+        """ Move through the next colour (but only up to a rows worth) """
+        current_colour = self._find_current_colour()
+        if current_colour is None:
+            self._update_y()
+            if not self._within_bounds():
+                raise StopIteration
+            current_colour = self._find_current_colour()
+        for cell in self._row_generator():
+            if not cell.stitched and cell.dmc_value == current_colour.dmc_value:
+                yield cell
+
+    def _within_bounds(self):
+        return 0 <= self.current_y < len(self.pattern)
+
+    def _find_current_colour(self):
+        """ Finds the first colour in this row that hasn't been stitched. """
+        for cell in self._row_generator():
+            if not cell.stitched:
+                return cell
+        return None
+
+    def _row_generator(self):
+        """ A generator function that goes through the entire row."""
+        # if self.horizontal_dir == HorizontalDirection.LEFT
         if self.horizontal_dir == HorizontalDirection.LEFT:
             while self.current_x < len(self.pattern[self.current_y]):
                 yield self.pattern[self.current_y][self.current_x]
                 self.current_x += 1
-            self.current_x = 0
         else:
             while self.current_x >= 0:
                 yield self.pattern[self.current_y][self.current_x]
                 self.current_x -= 1
-            self.current_x = len(self.pattern[0]) - 1
+        self.current_x = self._horizontal_idx_start()
+
+    def _update_y(self):
         if self.vertical_dir == VerticalDirection.TOP:
             self.current_y += 1
         else:
             self.current_y -= 1
+
+    def _horizontal_idx_start(self):
+        return 0 if self.horizontal_dir == HorizontalDirection.LEFT else len(self.pattern[0]) - 1
 
 
 TOP_LEFT = StartingCorner("top left", VerticalDirection.TOP, HorizontalDirection.LEFT)
--- tests/pattern_cells/test_started_from.py
+++ tests/pattern_cells/test_started_from.py
@@ -2,7 +2,7 @@ import pytest
 
 from pattern_cells.pattern_cell import PatternCell
 from pattern_cells.stitching_cell import StitchingCell
-from stitchers.starting_corner import PatternGenerator, TOP_LEFT, BOTTOM_LEFT, TOP_RIGHT, BOTTOM_RIGHT
+from stitchers.starting_corner import PatternGenerator, TOP_LEFT, BOTTOM_RIGHT, TOP_RIGHT, BOTTOM_LEFT
 
 
 # TODO: move to overall test file and import?
@@ -11,7 +11,7 @@ def create_stitching_cell(value: str):
 
 
 @pytest.fixture
-def pattern():
+def pattern_unique():
     return [
         [create_stitching_cell("a"), create_stitching_cell("b"), create_stitching_cell("c")],
         [create_stitching_cell("d"), create_stitching_cell("e"), create_stitching_cell("f")],
@@ -19,62 +19,124 @@ def pattern():
     ]
 
 
-def test_pattern_generator_row_top_left(pattern):
-    gen = PatternGenerator(TOP_LEFT, pattern)
-    result = [cell for cell in gen.move_through_row()]
-    assert len(result) == 3
-    assert result[0] == pattern[0][0]
-    assert result[1] == pattern[0][1]
-    assert result[2] == pattern[0][2]
-
-    assert gen.current_y == 1
-    assert gen.current_x == 0
-
-
-def test_pattern_generator_row_top_left_second_row(pattern):
-    gen = PatternGenerator(TOP_LEFT, pattern)
-    result = [cell for cell in gen.move_through_row()]
-    # Repeating the call to iterate through the second row.
-    result = [cell for cell in gen.move_through_row()]
-    assert len(result) == 3
-    assert result[0] == pattern[1][0]
-    assert result[1] == pattern[1][1]
-    assert result[2] == pattern[1][2]
-    assert gen.current_y == 2
-    assert gen.current_x == 0
+@pytest.fixture
+def pattern_dups():
+    return [
+        [create_stitching_cell("a"), create_stitching_cell("a"),
+         create_stitching_cell("b"), create_stitching_cell("b")],
+        [create_stitching_cell("b"), create_stitching_cell("b"),
+         create_stitching_cell("b"), create_stitching_cell("a")],
+        [create_stitching_cell("a"), create_stitching_cell("b"),
+         create_stitching_cell("a"), create_stitching_cell("b")]
+    ]
 
 
-def test_pattern_generator_row_bottom_left(pattern):
-    gen = PatternGenerator(BOTTOM_LEFT, pattern)
-    result = [cell for cell in gen.move_through_row()]
-    assert len(result) == 3
-    assert result[0] == pattern[2][0]
-    assert result[1] == pattern[2][1]
-    assert result[2] == pattern[2][2]
-
-    assert gen.current_y == 1
-    assert gen.current_x == 0
+@pytest.mark.parametrize(
+    ("current_y", "within_bounds"),
+    [(0, True), (1, True), (2, True), (-1, False), (3, False)])
+def test_within_bounds(current_y, within_bounds, pattern_unique):
+    gen = PatternGenerator(TOP_LEFT, pattern_unique)
+    gen.current_y = current_y
+    assert gen._within_bounds() == within_bounds
 
 
-def test_pattern_generator_row_top_right(pattern):
-    gen = PatternGenerator(TOP_RIGHT, pattern)
-    result = [cell for cell in gen.move_through_row()]
-    assert len(result) == 3
-    assert result[0] == pattern[0][2]
-    assert result[1] == pattern[0][1]
-    assert result[2] == pattern[0][0]
-
-    assert gen.current_y == 1
-    assert gen.current_x == 2
+@pytest.mark.parametrize(
+    ("starting_corner", "expected_colour"),
+    [(TOP_LEFT, "a"), (BOTTOM_RIGHT, "b")]
+)
+def test_find_current_colour_no_stitches(starting_corner, expected_colour, pattern_dups):
+    gen = PatternGenerator(starting_corner, pattern_dups)
+    assert gen._find_current_colour().dmc_value == expected_colour
 
 
-def test_pattern_generator_row_bottom_right(pattern):
-    gen = PatternGenerator(BOTTOM_RIGHT, pattern)
-    result = [cell for cell in gen.move_through_row()]
-    assert len(result) == 3
-    assert result[0] == pattern[2][2]
-    assert result[1] == pattern[2][1]
-    assert result[2] == pattern[2][0]
+@pytest.mark.parametrize(
+    ("starting_corner", "expected_colour", "stitched_values"),
+    [(TOP_LEFT, "b", [True, True, False, False]), (TOP_RIGHT, "a", [False, False, True, True])]
+)
+def test_find_current_colour_one_stitched(starting_corner, expected_colour, stitched_values, pattern_dups):
+    for cell, stitch_v in zip(pattern_dups[0], stitched_values):
+        cell.stitched = stitch_v
+    gen = PatternGenerator(starting_corner, pattern_dups)
+    assert gen._find_current_colour().dmc_value == expected_colour
 
-    assert gen.current_y == 1
-    assert gen.current_x == 2
+
+@pytest.mark.parametrize(
+    ("starting_corner", "expected_symbol_list"),
+    [(TOP_LEFT, ["a", "b", "c"]),
+     (TOP_RIGHT, ["c", "b", "a"]),
+     (BOTTOM_LEFT, ["g", "h", "i"]),
+     (BOTTOM_RIGHT, ["i", "h", "g"])]
+)
+def test_row_generator(starting_corner, expected_symbol_list, pattern_unique):
+    gen = PatternGenerator(starting_corner, pattern_unique)
+    assert [x.dmc_value for x in [cell for cell in gen._row_generator()]] == expected_symbol_list
+
+
+@pytest.mark.parametrize(
+    ("starting_corner", "expected_y_1", "expected_y_2"),
+    [(TOP_LEFT, 1, 2), (BOTTOM_LEFT, 1, 0)]
+)
+def test_update_y(starting_corner, expected_y_1, expected_y_2, pattern_unique):
+    gen = PatternGenerator(starting_corner, pattern_unique)
+    gen._update_y()
+    assert gen.current_y == expected_y_1
+    gen._update_y()
+    assert gen.current_y == expected_y_2
+
+
+@pytest.mark.parametrize(
+    ("starting_corner", "expected_horiz_idx"),
+    [(TOP_LEFT, 0), (TOP_RIGHT, 2)]
+)
+def test_horizontal_idx_start(starting_corner, expected_horiz_idx, pattern_unique):
+    gen = PatternGenerator(starting_corner, pattern_unique)
+    assert gen._horizontal_idx_start() == expected_horiz_idx
+
+
+@pytest.mark.parametrize(
+    ("starting_corner", "starting_y", "expected_symbols", "next_y", "next_x"),
+    [(TOP_LEFT, 0, ["a", "b", "c"], 1, 0),
+     (TOP_RIGHT, 1, ["f", "e", "d"], 2, 2),
+     (BOTTOM_LEFT, 2, ["g", "h", "i"], 1, 0),
+     (BOTTOM_RIGHT, 0, ["c", "b", "a"], -1, 2)]
+)
+def test_move_through_row(starting_corner, starting_y, expected_symbols, next_y, next_x, pattern_unique):
+    gen = PatternGenerator(starting_corner, pattern_unique)
+    gen.current_y = starting_y
+    assert [x.dmc_value for x in [cell for cell in gen.move_through_row()]] == expected_symbols
+    assert gen.current_y == next_y
+    assert gen.current_x == next_x
+
+
+# TODO: add test for stop iteration exception.
+
+@pytest.mark.parametrize(
+    ("starting_corner", "starting_y", "expected_symbol", "expected_number"),
+    [(TOP_LEFT, 0, "a", 2),
+     (TOP_RIGHT, 0, "b", 2),
+     (BOTTOM_RIGHT, 1, "a", 1),
+     (BOTTOM_LEFT, 1, "b", 3)]
+)
+def test_move_through_colour_in_rows(starting_corner, starting_y, expected_symbol, expected_number, pattern_dups):
+    gen = PatternGenerator(starting_corner, pattern_dups)
+    gen.current_y = starting_y
+    result = [cell for cell in gen.move_through_colour_in_rows()]
+    assert result[0].dmc_value == expected_symbol
+    assert len(result) == expected_number
+
+
+@pytest.mark.parametrize(
+    ("starting_corner", "expected_symbol", "expected_number"),
+    [(TOP_LEFT, "b", 3),
+     (BOTTOM_RIGHT, "a", 1)]
+)
+def test_move_through_colour_in_rows_change_row(starting_corner, expected_symbol, expected_number, pattern_dups):
+    gen = PatternGenerator(starting_corner, pattern_dups)
+    # Call move through colour twice and mark each returned cell as stitched to force a row change
+    for _ in range(2):
+        for cell in gen.move_through_colour_in_rows():
+            cell.stitched = True
+
+    result = [cell for cell in gen.move_through_colour_in_rows()]
+    assert result[0].dmc_value == expected_symbol
+    assert len(result) == expected_number
