6143ae942a46806735839a53cf62a8a4c825b55e
Madeleine Wagner <musomaddy@gmail.com>
2023-04-16 12:38:52 +1000

    Created card layout for the main page.

--- gui/layouts/current_page.py
+++ gui/layouts/current_page.py
@@ -1,7 +1,9 @@
-from PyQt6.QtWidgets import QHBoxLayout, QLabel, QWidget
+from PyQt6.QtWidgets import QHBoxLayout, QLabel
+
+from gui.layouts.styled_widget import StyledWidget
 
 
-class CurrentPageLayout(QHBoxLayout):
+class _CurrentPageLayout(QHBoxLayout):
     """ ???? No idea if this is really the layout I want but I'll figure it out. """
 
     def __init__(self):
@@ -10,12 +12,12 @@ class CurrentPageLayout(QHBoxLayout):
         self.addWidget(QLabel("Current page contents :)"))
 
 
-class CurrentPage(QWidget):
+class CurrentPage(StyledWidget):
     """
     Holds the content of the current page. Sits in between contents and something else - dunno what yet.
     """
 
     def __init__(self):
-        super().__init__()
+        super().__init__("page")
 
-        self.setLayout(CurrentPageLayout())
+        self.setLayout(_CurrentPageLayout())
